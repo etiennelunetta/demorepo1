7307f9f545633c738d04b973ff23c104b9171fbc
ZeroPath <noreply@zeropath.com>
2025-09-03 20:22:14 +0000

    fix: disable executeUserScript to prevent XSS vulnerability

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ function displayUserInput() {
 
 // 2. Insecure use of eval
 function executeUserScript() {
-    var userScript = document.getElementById('userScript').value;
-    // Using eval to execute user-provided script
-    eval(userScript);
+    console.warn('executeUserScript is disabled for security reasons');
 }
 
 // 3. Unsecured AJAX request
